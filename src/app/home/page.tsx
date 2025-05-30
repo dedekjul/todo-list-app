9bb9eec767f4245ce8a6853d472ccf3e59497d87
dekjul <210040076@stikom-bali.ac.id>
2025-05-30 11:36:28 +0800

    implement home ui page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,46 @@
+import TodoItem from "@/components/TodoItem";
+
+export default function HomePage() {
+  const handleDelete = (id: string) => {
+    console.log(`Delete todo with id: ${id}`);
+  };
+
+  const handleMarkDone = (id: string) => {
+    console.log(`Mark todo with id: ${id} as done`);
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center bg-neutral-700">
+      <div className="max-w-md px-8 py-10 space-y-6 relative">
+        <h1 className="text-3xl font-bold text-white mb-6">Hi, John Doe</h1>
+
+        {/* Todo List Cards */}
+        <TodoItem
+          title="Submit Code to Taksu "
+          dueDate="21 October 2021 07:30PM"
+          status="OPEN"
+          // onDelete={() => handleDelete('1')}
+          // onMarkDone={() => handleMarkDone('1')}
+        />
+        <TodoItem
+          title="Submit Code to Taksu blabla awdawdn dajwbf  dajwbdibaef"
+          dueDate="21 October 2021 07:30PM"
+          status="OPEN"
+          // onDelete={() => handleDelete('1')}
+          // onMarkDone={() => handleMarkDone('1')}
+        />
+        <TodoItem
+          title="Submit Code to Taksu blabla awdawdn dajwbf  dajwbdibaef"
+          dueDate="21 October 2021 07:30PM"
+          status="OPEN"
+          // onDelete={() => handleDelete('1')}
+          // onMarkDone={() => handleMarkDone('1')}
+        />
+
+        <button className="absolute bottom-8 right-8 bg-green-400 text-neutral-700 py-4 px-6 text-4xl rounded-full shadow-lg">
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
